af91a6011c2248c22e40ad509f70cf2ee6d4e88d
Marc Raymond A. Serrano <marc.serrano8779@gmail.com>
2020-10-12 20:12:39 -0700

    Create Yogurt.java

---
+++ KickStartTutorial/Yogurt.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io*;
+
+public class Solution
+{
+    public static void main(String args[])
+    {
+        try(Scanner in = new Scanner())
+        {
+            int cases = in.nextInt();
+            for(int i = 0; i < casesl i++)
+            {
+                int n = in.nextInt();
+                int k = in.nextInt();
+                
+                int[] buckets = new int[n+1];
+                
+                for(int i = 0; i < n; i++)
+                {
+                    
+                }
+            
+            }
+        }
+    }
+}
